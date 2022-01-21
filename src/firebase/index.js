f75bf29e6cc710be8f3cb1aa7c261b305057cfab
Daniel Palmdal <daniel.palmdal@cdon.com>
2022-01-21 23:57:14 +0100

    adding config for firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,33 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+	storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+	appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  };
+
+// init firebase
+const app = initializeApp(firebaseConfig)
+
+// get firebase auth instance
+const auth = getAuth()
+
+// get firebase firestore instance
+const db = getFirestore(app)
+
+// get firebase storage instance
+const storage = getStorage(app)
+
+
+export {
+	app as default,
+	auth,
+	db,
+	storage
+}

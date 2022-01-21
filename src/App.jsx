e7c48421a948fb4f07046562c96734c1af7cb26c
Daniel Palmdal <daniel.palmdal@cdon.com>
2022-01-21 20:42:09 +0100

    adding drawer to navigation

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage"
 import ProfilePage from "./pages/ProfilePage"
 import LoginPage from "./pages/LoginPage"
+import Navigation from "./pages/partials/Navigaton"
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/profile" element={<ProfilePage />} />
---
+++ src/pages/partials/Navigaton.jsx
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+
+import Button from "@mui/material/Button";
+
+import MenuItem from "@mui/material/MenuItem";
+import Drawer from "@mui/material/Drawer";
+
+const isLoggedIn = true;
+
+const pages = [
+  { page: "Page 1", path: "/" },
+  { page: "Page 2", path: "/" },
+  { page: isLoggedIn ? "Logout" : "Login", path: "/profile" },
+];
+
+const ResponsiveAppBar = () => {
+  const [state, setState] = useState(false);
+
+  const toggleDrawer = (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState(!state);
+  };
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          {/* Navigation for hamburger menu  */}
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "flex", md: "none" },
+              justifyContent: "end",
+            }}
+          >
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              onClick={toggleDrawer}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+
+            <Drawer
+              sx={{
+                display: { md: "none" },
+              }}
+              anchor={"right"}
+              open={state}
+              onClose={toggleDrawer}
+            >
+              {pages.map((page, i) => (
+                <Box key={i} onClick={toggleDrawer} sx={{ width: 250 }}>
+                  <MenuItem>
+                    <Typography as={Link} to={page.path} textAlign="center">
+                      {page.page}
+                    </Typography>
+                  </MenuItem>
+                </Box>
+              ))}
+            </Drawer>
+          </Box>
+
+          {/* Navigation for hamburger menu end */}
+
+          {/* Navigation for normal menu  */}
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "none", md: "flex" },
+              justifyContent: "end",
+            }}
+          >
+            {pages.map((page, i) => (
+              <Button key={i} sx={{ my: 2, color: "white", display: "block" }}>
+                <Link to={page.path}>{page.page}</Link>
+              </Button>
+            ))}
+          </Box>
+          {/* Navigation for normal menu end */}
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+export default ResponsiveAppBar;

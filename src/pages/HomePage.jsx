38ad07b21ab1d6f551b8ea09bf47b324225c63a9
Daniel Palmdal <daniel.palmdal@cdon.com>
2022-01-21 18:10:39 +0100

    adding several pages

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,6 @@
+
+const HomePage = () => {
+  return <div> hello </div>;
+};
+
+export default HomePage;
---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,9 @@
+const ProfilePage = () => {
+  return (
+    <div>
+      <h1>welcome to the profilepage</h1>
+    </div>
+  );
+};
+
+export default ProfilePage;

16d74f430d1c969f834ff819e6acfc25401ccbfc
Daniel Palmdal <daniel.palmdal@cdon.com>
2022-02-19 08:32:00 +0100

    adding fundamental pages

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import RegisterPage from "./pages/RegisterPage";
 import Navigation from "./pages/partials/Navigaton";
 import LogoutPage from "./pages/LogoutPage";
 import SpotsPage from "./pages/SpotsPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 //components
 import IsProtected from "./components/IsProtected";
@@ -25,11 +26,20 @@ function App() {
             </IsProtected>
           }
         />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route
+          path="/profile"
+          element={
+            <IsProtected redirectTo="/login">
+              <ProfilePage />
+            </IsProtected>
+          }
+        />
+
+        <Route path="/logout" element={<LogoutPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/spots" element={<SpotsPage />} />
-        {/* <Route path="*" element={<NotFoundPage />} /> */}
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </div>
   );
---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,5 @@
+const NotFoundPage = () => {
+  return <h1>Sorry the page could not be found</h1>;
+};
+
+export default NotFoundPage;

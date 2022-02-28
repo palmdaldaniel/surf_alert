ad4c9fffe18b8a7b4a5e60af9af15bdbf31c8d4d
Daniel Palmdal <daniel.palmdal@cdon.com>
2022-02-28 01:04:15 +0100

    fixed temp api

--- src/services/SMHI_API.js
+++ src/services/SMHI_API.js
@@ -1,12 +1,7 @@
 import axios from "axios";
 
-const get = async (endpoint) => {
-  const result = await axios.get(endpoint);
-  return result;
-};
-
 export const getWaterTemp = async (stationId) => {
-  const result = await get(
+  const result = await axios.get(
     `https://opendata-download-ocobs.smhi.se/api/version/latest/parameter/5/station/${stationId}/period/latest-hour/data.json`
   );
 
